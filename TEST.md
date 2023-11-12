52f64f41528f5ed2b50b4080f38a898360fb9417
José Cleiton Correia <jcleitonbc@gmail.com>
2023-11-12 13:09:46 -0300

    test: add benchmark

---
+++ TEST.md
@@ -0,0 +1,10 @@
+# Test
+
+Testing commands for people not familiar with golang or golang testing framework
+
+```sh
+go test ./...
+
+# Benchmarking
+go test -bench . -benchmem -count 10 ./...
+```
---
+++ test/app/game/play_benchmark_test.go
@@ -0,0 +1,76 @@
+package game
+
+import (
+	"testing"
+
+	"github.com/josecleiton/domino/app/game"
+	"github.com/josecleiton/domino/app/models"
+)
+
+func BenchmarkTestPlayGlue(b *testing.B) {
+	gameStateSt := firstPlay()
+	play, err := game.Play(&gameStateSt)
+	if err != nil {
+		b.Fatal("Error on first play")
+	}
+
+	plays := []models.DominoPlay{
+		{
+			PlayerPosition: play.PlayerPosition,
+			Bone:           *play.Bone,
+			Reversed:       play.Reversed,
+		},
+		{
+			PlayerPosition: play.PlayerPosition + 1,
+			Bone: models.Domino{
+				X: 5,
+				Y: 4,
+			},
+			Reversed: false,
+		},
+		{
+			PlayerPosition: play.PlayerPosition + 2,
+			Bone: models.Domino{
+				X: 5,
+				Y: 3,
+			},
+			Reversed: false,
+		},
+	}
+
+	table := make(map[int]map[int]bool, len(plays))
+	for _, play := range plays {
+		if _, ok := table[play.Bone.X]; !ok {
+			table[play.Bone.X] = make(map[int]bool, len(plays))
+		}
+		if _, ok := table[play.Bone.Y]; !ok {
+			table[play.Bone.Y] = make(map[int]bool, len(plays))
+		}
+		table[play.Bone.X][play.Bone.Y] = true
+		table[play.Bone.Y][play.Bone.X] = true
+	}
+
+	newHand := make([]models.Domino, 0, len(gameStateSt.Hand)-1)
+	for _, bone := range gameStateSt.Hand {
+		if bone != *play.Bone {
+			newHand = append(newHand, bone)
+		}
+	}
+
+	gameStateNd := models.DominoGameState{
+		PlayerPosition: play.PlayerPosition,
+		Plays:          plays,
+		Hand:           newHand,
+		Table:          table,
+	}
+
+	play, err = game.Play(&gameStateNd)
+	if err != nil {
+		b.Fatal("Error on play")
+	}
+
+	if play.Pass() {
+		b.Fatal("Pass is not allowed on glue play")
+	}
+
+}
